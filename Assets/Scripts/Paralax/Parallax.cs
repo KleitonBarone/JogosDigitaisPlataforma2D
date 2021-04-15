2f9bec022795dd4a5578f98ad69a7e47d8462e5b
leohuginn <leonardo.oliveira@outlook.com>
2021-04-15 15:57:25 -0300

    Iniciando construção do mapa, configurado tile Pallet para desenho do mesmo. Realizada também acerca de cenário, implementação da parallax para renderização de cenário de fundo

---
+++ Assets/Scripts/Paralax/Parallax.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    //public GameObject playerCamera1 = GameObject.Find("playerCamera");
+    public GameObject playerCamera;
+    public float length, startPos;
+    public float parallaxSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        float temp = (playerCamera.transform.position.x * (1 - parallaxSpeed));
+        float distance = (playerCamera.transform.position.x * parallaxSpeed);
+
+        transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+
+        if (temp > startPos + length)
+        {
+            startPos += length;
+        }
+        else if (temp < startPos - length)
+        {
+            startPos -= length;
+        }
+    }
+}
